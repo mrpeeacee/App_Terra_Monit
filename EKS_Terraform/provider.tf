d0d493d857f4975f0a290e55108bb7ce1d073a43
mrpeeacee <nikhilgbhat007.com>
2025-11-08 12:59:37 +0530

    changed provider.tf

--- EKS_Terraform/provider.tf
+++ EKS_Terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "gandalf-web-app-terraform-eks-state-s3-bucket-4565"
+    bucket         = "gandalf-web-app-terraform-eks-state-s3-bucket-6969"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
